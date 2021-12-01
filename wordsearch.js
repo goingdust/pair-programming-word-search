20adfc1766fe15634f1960981bb54364ad6b6006
goingdust <dustinluck@gmail.com>
2021-12-01 19:10:07 +0000

    Used the transpose function to get the columns of the word search so it can return whether the word appears vertically as well as horizontally

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  const finalArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let holderArray = [];
+    for (let j = 0; j < matrix.length; j++) {
+      holderArray.push(matrix[j][i]);
+    }
+    finalArray.push(holderArray);
+  }
+  return finalArray;
+};
+
+module.exports = wordSearch;
